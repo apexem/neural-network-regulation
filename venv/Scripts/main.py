8218c5862727afe2a008edcf2b4e3019a0168076
Spartadays <watly.mateusz@gmail.com>
2019-03-06 19:34:22 +0100

    test

--- venv/Scripts/main.py
+++ venv/Scripts/main.py
@@ -3,6 +3,7 @@ from scipy import signal
 import matplotlib.pyplot as plt
 import control
 
+
 class DCMotor:
 
     def __init__(self, voltage, resistance,
@@ -12,15 +13,14 @@ class DCMotor:
         self.V = voltage
         self.R = resistance
         self.L = inductance
-        self.J = inertia # moment of inertia of the rotor
-        self.b = friction # viscous friction constant
-        self.Ke = electromotive_force # electromotive force constant
-        self.Km = torque # motor torque constant
+        self.J = inertia  # moment of inertia of the rotor
+        self.b = friction  # viscous friction constant
+        self.Ke = electromotive_force  # electromotive force constant
+        self.Km = torque  # motor torque constant
 
-        numerator = [self.Ke] # no load, so motor torque equals electromotive force
-        denominator = [self.J* self.L, self.J*self.R+self.L*self.b, self.R*self.b + self.Ke*self.Ke]
+        numerator = [self.Ke]  # no load, so motor torque equals electromotive force
+        denominator = [self.J*self.L, self.J*self.R+self.L*self.b, self.R*self.b + self.Ke*self.Ke]
         self.transfer_function = signal.TransferFunction(numerator, denominator)
-
 
     def plot_step_response(self):
         t, y = signal.step2(self.transfer_function)
@@ -33,8 +33,7 @@ def __main__():
     motor = DCMotor(voltage=1, resistance=0.5, inductance=0.01, inertia=0.1,
                     friction=0.01, torque=0.01, electromotive_force=0.01)
     motor.plot_step_response()
-
-
+    print("Test pusha github xDD")
 
 
 if __name__ == "__main__":
